1c4a645604f5269e6705b6ced3408cb963d3896b
cyrill olano <2389891@bdeb.qc.ca>
2024-05-30 17:27:28 -0400

    format et javascript

---
+++ page.js
@@ -0,0 +1,19 @@
+function changerCouleur() {
+    var titre = document.getElementById("titre");
+    var currentColor = window.getComputedStyle(titre).color;
+    if (currentColor === 'rgb(0, 0, 255)') {
+        titre.style.color = 'green';
+    } else {
+        titre.style.color = 'blue';
+    }
+}
+
+function agrandir() {
+    document.getElementById("titre").style.fontSize = '60px'
+}
+function diminuer(){
+    document.getElementById("titre").style.fontSize = '20px'
+}
+function effacer(){
+    document.getElementById("titre").innerHTML = ""
+}
